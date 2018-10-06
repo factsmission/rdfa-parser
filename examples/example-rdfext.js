029d0d9f33d6dac81e24b42b77ce756c0a7a191a
Noam Bachmann <noam@helou.ch>
2018-10-06 13:26:05 +0200

    showing extracted data also on the bottom of the rdf-ext example

--- examples/example-rdfext.js
+++ examples/example-rdfext.js
@@ -1,9 +1,21 @@
 // Parsing as Element
 window.onload = () => {
     let g = rdf.graph();
+    //Following 3 lines can be removed if the Note gets removed.
+    let note = document.querySelector("#note");
+    note.style.backgroundColor = "#ffab91";
+    note.style.padding = "8px";
+    let result = document.createElement("div");
+    result.appendChild(document.createElement("h4")).innerHTML = "Triples found:"
+    result.style.backgroundColor = "#eeeeee";
+    result.style.padding = "8px";
     RDFa.parseDOM(document.documentElement, (quad) => g.add(quad));
     const output = (new NTriplesSerializer()).import(g.toStream());
     output.on('data', ntriples => {
-        console.log(ntriples.toString())
+        let code = document.createElement("code");
+        code.innerText = ntriples.toString();
+        result.appendChild(code);
+        document.querySelector("body").appendChild(result);
+        console.log(ntriples.toString());
       });
 }
