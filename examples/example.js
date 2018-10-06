bb7521ff3b6fc4b5699849b466b426bfbc574c3e
Noam Bachmann <noam@helou.ch>
2018-10-06 13:08:16 +0200

    showing extracted data on the bottom of the page

--- examples/example.js
+++ examples/example.js
@@ -1,13 +1,21 @@
 // Parsing as Element
 window.onload = () => {
     let g = $rdf.graph();
+    let result = document.createElement("div");
+    result.appendChild(document.createElement("h4")).innerHTML = "Triples found:"
+    result.style.backgroundColor = "#eeeeee";
+    result.style.padding = "8px";
     RDFa.parseDOM(document.documentElement, (quad) => {
         console.log(quad);
         g.add(quad);
     });
     const output = $rdf.serializers["text/turtle"].import(g.toStream());
     output.on('data', ntriples => {
-        console.log(ntriples.toString())
+        console.log(ntriples.toString());
+        let code = document.createElement("code");
+        code.innerText = ntriples.toString();
+        result.appendChild(code);
+        document.querySelector("body").appendChild(result);
       })
     //console.log(g.toNT());
 }
